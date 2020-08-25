9ea5197664a219c5efd5b53bcad3fac13ba271e0
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-25 10:31:38 -0400

    can now run from command line

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// #!/Users/stevenmcgrath/.nvm/versions/node/v12.18.3/bin/node
+
+const fs = require('fs');
+
+fs.readdir(process.cwd(), { withFileTypes: true }, (err, files) => {
+  files.map((file) => {
+    console.log(file);
+  });
+});
