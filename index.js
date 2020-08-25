3748cae04fc9e673efc3c43bb656b20c7874c4c6
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-25 10:53:34 -0400

    added colorized outputs

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
-// #!/Users/stevenmcgrath/.nvm/versions/node/v12.18.3/bin/node
 
 const fs = require('fs');
+const colors = require('colors');
 
-fs.readdir(process.cwd(), (err, files) => {
+const requestedPath = process.argv[2];
+
+fs.readdir(requestedPath || process.cwd(), { withFileTypes: true }, (err, files) => {
+  if (err) {
+    throw new Error(err);
+  }
   files.map((file) => {
-    console.log(file);
+    fs.lstat(file.name, (err, stats) => {
+      if (file.name[0] === '.') {
+        console.log(file.name.red);
+      } else if (stats.isDirectory()) {
+        console.log(file.name.blue);
+      } else if (stats.isFile()) {
+        console.log(file.name.green);
+      } else {
+        console.log(file.name);
+      }
+    });
   });
 });
