f05dc3f598b7a5783ec6e02175d6b17774cb6bca
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-25 11:10:59 -0400

    changed to promise based implementation

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 const fs = require('fs');
 const colors = require('colors');
+const { lstat } = fs.promises;
 
 const requestedPath = process.argv[2];
 
-fs.readdir(requestedPath || process.cwd(), { withFileTypes: true }, (err, files) => {
+fs.readdir(requestedPath || process.cwd(), async (err, files) => {
   if (err) {
     throw new Error(err);
   }
-  files.map((file) => {
-    fs.lstat(file.name, (err, stats) => {
-      if (file.name[0] === '.') {
-        console.log(file.name.red);
-      } else if (stats.isDirectory()) {
-        console.log(file.name.blue);
-      } else if (stats.isFile()) {
-        console.log(file.name.green);
-      } else {
-        console.log(file.name);
-      }
-    });
+  let statPromises = files.map((file) => {
+    return lstat(file);
   });
+  const allStats = await Promise.all(statPromises);
+  for (let stats of allStats) {
+    const idx = allStats.indexOf(stats);
+    const filename = files[idx];
+    if (filename[0] === '.') {
+      console.log(filename.red);
+    } else if (stats.isDirectory()) {
+      console.log(filename.blue);
+    } else {
+      console.log(filename.green);
+    }
+  }
 });
